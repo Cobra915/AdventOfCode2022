4025b953ddfb560e38154b7f115b8b2749639ef2
Cobra915 <crbradshaw93@gmail.com>
2022-12-26 16:34:21 -0600

    Updating day 7 after completing part 1

--- python/day7/day7part1/display_output.py
+++ python/day7/day7part1/display_output.py
@@ -1,7 +1,17 @@
 import pprint
 import json
 
-def display_output(File_Tree, Mem_For_Delete):    
+def display_output(File_Tree, Mem_For_Delete):
+
+    # with open('convert.txt', 'w') as convert_file:
+    #    for key, nested in sorted(File_Tree.items()):
+    #        print(key, file=convert_file)
+    #        for subkey, value in sorted(nested.items()):
+    #            print('   {}: {}'.format(subkey, value), file=convert_file)
+    #        print(file=convert_file)
+    
+
+    # print(f"The filesystem was written to {convert_file}")
     
     print(f"Total Memory to be deleted: {Mem_For_Delete}")
 
